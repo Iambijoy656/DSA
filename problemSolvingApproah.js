f6a2cb3179089724b5bf4c74163d9305de2314cc
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-12-16 18:33:23 +0600

    problem solving approach

---
+++ problemSolvingApproah.js
@@ -0,0 +1,13 @@
+/**
+    1. sum 2 numbers and return the result 
+    2. addition of two numbers
+    3. input - 10,15 output -25 
+    4. input 20,30 output -50
+    5. input 20.33 , 40.5345 output - 60
+ */
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+const result = sum(10, 20)
+console.log(result);
