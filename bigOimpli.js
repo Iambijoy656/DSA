16207a7a753d8ababad4e8f7639f639c067640b5
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-12-16 11:14:28 +0600

    add

--- bigOimpli.js
+++ bigOimpli.js
@@ -1,3 +1,17 @@
+
+// Big o Indicate:
+// How number of operation increases/relates with input
+// Best case describe(omega)
+// Average case (theta)
+// Worse case(BIG O)
+
+
+
+
+
+
+
+
 // linear Big O - O(n)
 
 // function log(n) {
@@ -103,7 +117,55 @@
 
 
 // Big(O) = O(1) /Constant
-function addItems(n) {
-    console.log(n + n + n)
+// function addItems(n) {
+//     console.log(n + n + n)
+// }
+// addItems(20)
+
+
+
+
+
+
+// O(41) best case
+// O(n) worse case
+// function logAtLeast(n) {
+//     for (let i = 0; i < Math.max(5, n); i++) {
+//         console.log(i)
+//     }
+// }
+
+// logAtLeast(3)
+
+
+
+
+
+// O(1) best
+// O(n)
+// function logAtLeast5(n) {
+//     for (let i = 0; i < Math.min(5, n); i++) {
+//         console.log(i)
+//     }
+// }
+
+// logAtLeast5(6)
+
+
+
+
+// O(m+n)
+function log(a, b) {
+    for (let i = 0; i < a; i++) {
+        console.log(a)
+    }
+
+    for (let k = 0; k < b; k++) {
+        console.log(b);
+
+    }
 }
-addItems(20)
+log(2, 5)
+
+
+
