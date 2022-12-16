f04e0dfee9419d53f6dfb81ec34224a2450a6491
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-12-16 13:31:56 +0600

    array and obj performance

---
+++ arrAndObjPerformance.js
@@ -0,0 +1,17 @@
+/**
+ * Array and object(Performance) - BIG(O)
+ * How to approach a problem
+ */
+
+const arr = ['a', 'b', 'c'];
+console.log(arr.length);
+
+// access by index O(1) [time complexity]
+console.log(arr[arr.length - 1])
+
+
+// adding item/element
+
+// arr[arr.length] = 'd';
+arr.push('d')
+console.log(arr);
