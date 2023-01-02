4b30621925ace4c4c64f721089291408cd1fede4
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2023-01-03 01:23:12 +0600

    pattern

--- shap and pattern/pattern.js
+++ shap and pattern/pattern.js
@@ -35,3 +35,50 @@ steps(4)
 
 // }
 // steps(3)
+
+
+
+
+
+// Pyramid pattern
+
+/**
+ * write a function that accepts a positive number N.
+ * The function should console log a pyramid shape with N levels using the # character . make sure the pyramid has spaces on both the left and right hand side
+ 
+----Example------
+pyramid (1)
+   "#"
+ pyramid (2)
+   " # "
+   "###"
+
+pyramid (3)
+   " # "
+  " ### "
+  "#####"
+
+ */
+
+
+function pyramid(n) {
+    let midPoint = Math.floor((2 * n - 1) / 2)
+    for (let row = 0; row < n; row++) {
+        let level = ''
+        for (let column = 0; column < 2 * n - 1; column++) {
+            // console.log(column)
+            if (midPoint - row <= column && midPoint + row >= column) {
+                level += "#"
+
+            } else {
+                level += " "
+            }
+
+
+        }
+        console.log(level);
+
+    }
+
+}
+pyramid(5)
